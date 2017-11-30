fb131ad4e38f46f919adcb1fa8faee9df0121a39
James Ferguson <ncjamesferguson@gmail.com>
2017-11-30 23:08:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,26 +2,26 @@
 
 def take_a_number(line, name)
   line.push(name)
-   "Welcome, #{name}. You are number #{line.length} in line."
+   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
 
 def now_serving(line)
   if (line.length == 0)
-    "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
-    "Currently serving #{line.shift}."
+    puts "Currently serving #{line.shift}."
   end
 end
 
 def line(line)
 
   if (!line.length)
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
     message = "The line is currently: 1. #{line[0]}"
       line.length times do
         message += ", ${i + 1}. #{line[i]}"
       end
-    message;
+    puts message;
   end
 end
